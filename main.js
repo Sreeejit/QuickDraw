1ab799c3b303269f9fd6cf8495c3d898a7ff94db
Sreeejit <77166984+Sreeejit@users.noreply.github.com>
2022-02-11 17:16:08 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,21 +3,70 @@ function setup(){
     canvas = createCanvas(280, 280);
     canvas.center();
     background('white');
+    canvas.mouseReleased(classifyCanvas);}
+random_number = Math.floor((Math.random() * quick_draw_data_set.length)+1);
+console.log(quick_draw_data_set[random_number]);
+sketch = quick_draw_data_set[random_number];
+document.getElementById('sketch_name').innerHTML = "Sketch to be Drawn : " + sketch;
 
-    
-random_number = floor (random(0,quick_draw_data_set.length));
-console.log("Sketch To Be Drawn: " + random_number);
-sketch = random_number;}
-imer_counter = 0;
-timer_check = [];
-drawn_sketch = [];
-answer_holder = [];
+timer_counter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
 score = 0;
 
 function check_sketch(){
-
+timer_counter++;
+document.getElementById("TimerSpan").innerHTML = 'Timer : ' + timer_counter;
+console.log(timer_counter);
+if(timer_counter>800){
+timer_counter = 0;
+timer_check = "completed";
+}
+if(timer_check == "completed"||answer_holder=="set"){
+timer_check = "";
+answer_holder = "";
+updateCanvas();
+}
 }
 
 function updateCanvas(){
+    background("white"); 
+    random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1); 
+    console.log(quick_draw_data_set[random_number]); 
+    sketch = quick_draw_data_set[random_number]; 
+    document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+}
 
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet')
+}
+
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    
+    if(mouseIsPressed){
+    line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+    check_sketch()
+    if(drawn_sketch == sketch){
+    answer_holder = "set";
+    score++;
+    document.getElementById("ScoreSpan").innerHTML = 'Score : ' + score;
+    }
+}
+
+function classifyCanvas(){
+classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementById('label').innerHTML = 'Your Sketch : ' + drawn_sketch;
+    document.getElementById('confidence').innerHTML = 'Confidence : ' + Math.round(results[0].confidence * 100) + '%';
 }
